e82ca8c4512a41f4b5fd633f855f4f9102ed809f
deborah reis <deborahmreis3@gmail.com>
2025-06-06 12:00:12 +0100

    Add handlePostgresErrors in app.js

--- app.js
+++ app.js
@@ -20,5 +20,15 @@ app.get('/api/articles', getAllArticles);
 
 app.get('/api/users', getAllUsers);
 
+const handlePostgresErrors = (err, request, response, next) => {
+  // vou escrever aqui o codigo de erro do postgres
+  if(err.code === "22P02") {
+    response.status(400).send( { msg: "bad request"})
+  } else {
+    next(err)
+  }
+};
+
+app.use(handlePostgresErrors)
 
 module.exports = app;
